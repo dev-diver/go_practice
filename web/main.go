6824e23d49e549387b5161839df6be005bec6606
SoKyungHyun <valentine92exp@gmail.com>
2024-06-07 15:37:59 +0900

    gorilla mux

--- web/main.go
+++ web/main.go
@@ -3,15 +3,25 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello World")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/member/{name}/age/{age}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		name := vars["name"]
+		age := vars["age"]
+
+		fmt.Fprintf(w, "member %s 's profile in %s\n", name, age)
 	})
 
 	fs := http.FileServer(http.Dir("file/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", fs))
+	// .Handle이 http는 /로 끝나면 이하 모든 url을 핸들링하지만,  mux는 정확히 일치하는 것만 핸들링
+	// 따라서 mux는 PathPrefix를 써야 한다.
 
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":80", r)
 }
